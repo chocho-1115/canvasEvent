5e777d4b3de250abf8310fcf6a6b06f0f49e0e43
chocho-1115 <849890769@qq.com>
2017-07-26 17:12:00 +0800

    完成disabled函数；添加对圆形事件区域的支持

--- canvasEvent.js
+++ canvasEvent.js
@@ -5,7 +5,8 @@
 	CE.Stage = function(canvas,regEvent){
 		
 		var self = this;
-			
+		var isTouch =  'ontouchstart' in document ? true : false;
+		
 		if(canvas.currentStyle){
 			this.borderLeftWidth = parseFloat(canvas.currentStyle['borderLeftWidth']);
 			this.borderTopWidth = parseFloat(canvas.currentStyle['borderTopWidth']);
@@ -14,20 +15,28 @@
 			this.borderTopWidth = parseFloat(getComputedStyle(canvas, null)['borderTopWidth']);
 		}
 		
+		this.deviceType = isTouch ? 'touch' : 'mouse';
 		this.canvas = canvas;
 		this.areas = [];
 		this.childLength = 0;
 		this.regEvent = {
-			down : false,
-			up : false,
-			click : false,
-			over : false,
-			out : false,
-			move : false
+			down : { status:false, name: 'mousedown'},
+			up : { status:false, name: 'mouseup'},
+			click : { status:false, name: 'click'},
+			over : { status:false, name: 'mousemove'},
+			out : { status:false, name: 'mousemove'},
+			move : { status:false, name: 'mousemove'}
 		};
 		
-		
-		
+		if(isTouch){
+			this.regEvent.down.name = 'touchstart';
+			this.regEvent.up.name = 'touchend';
+			
+			this.regEvent.over.name = 'touchmove';
+			this.regEvent.out.name = 'touchmove';
+			this.regEvent.move.name = 'touchmove';
+			
+		}
 		
 		self.enabled.apply(this,regEvent);
 	
@@ -38,98 +47,77 @@
 		//启用
 		enabled:function(){
 			var len = arguments.length,
-				self = this,
-				deviceType =  'ontouchstart' in document ? 'touch' : 'mouse';
+				self = this;
 
 			for(var i=0;i<len;i++){
-				var inEvent = '',
-					addEvent = '';
-				addEvent = inEvent = arguments[i];
+				var inEvent = arguments[i];
+				addEvent(inEvent);
+			}
+			
+			//给画布添加事件
+			function addEvent(inEvent){
 				
-				self.regEvent[inEvent] = true;
-				//防止重复启用move事件
-				switch(inEvent){
-					
-					case 'click':
-						if(self.canvas['onclick']){
-							continue;
-						}
-						addEvent = 'click';
-						break;
-					case 'down':
-						if(deviceType=='touch')inEvent = 'start'
-						if(self.canvas['on'+deviceType+inEvent]){
-							continue;
-						}
-						addEvent = deviceType+inEvent;
-						break;
-					case 'up':
-						if(deviceType=='touch')inEvent = 'end'
-						if(self.canvas['on'+deviceType+inEvent]){
-							continue;
-						}
-						addEvent = deviceType+inEvent;
-						break;
-					default :
-						if (inEvent=='over'||inEvent=='move'||inEvent=='out'){
-							if(self.canvas['on'+deviceType+'move']){
-								continue;
-							}
-							addEvent = deviceType+'move';
-						}else{
-							continue;
-						}
-					
+				if(!self.regEvent[inEvent])return false
+				
+				//防止事件重复注册
+				if(self.regEvent[inEvent].status)return false;
+				if (inEvent=='over'||inEvent=='move'||inEvent=='out'){
+					if(self.regEvent['over'].status||self.regEvent['move'].status||self.regEvent['out'].status){
+						self.regEvent[inEvent].status = true;
+						return false;
+					}
 				}
+				self.regEvent[inEvent].status = true;
 				
-				
-				
-				if(addEvent==deviceType+'move'){
-					
-					//text.innerHTML += 'ADD ' + addEvent + '|'
-					
-					self.canvas['on'+addEvent] = function(e){
+				//添加over out move 事件
+				if(self.regEvent[inEvent].name == self.deviceType + 'move'){
+					self.canvas['on'+self.regEvent[inEvent].name] = function(e){
 						var offset = self.getEventOffset(e);
 						for(var i=0;i<self.childLength;i++){
-							var isIn = self.areas[i].isInArea(offset.x,offset.y);
+							var thisAreas = self.areas[i];
+							var isIn = thisAreas.isInArea(offset.x,offset.y);
 							if(isIn){
-								if(self.areas[i].status==-1){
-									self.areas[i].status = 1;
-									e.type = deviceType+'over';
-									self.occur(self.areas[i]['onover'],e)
+								if(thisAreas.status==-1){
+									inEvent = 'over';
+									thisAreas.status = 1;
+									e.type = self.deviceType+'over';
+									if(self.regEvent[inEvent].status){
+										self.occur(thisAreas['onover'],e,thisAreas)
+									}
 								}else{
-									self.occur(self.areas[i]['onmove'],e)
+									inEvent = 'move';
+									if(self.regEvent[inEvent].status){
+										self.occur(thisAreas['onmove'],e,thisAreas)
+									}
 								}
 							}else{
-								if(self.areas[i].status==1){
-									self.areas[i].status = -1;
-									e.type = deviceType+'out';
-									self.occur(self.areas[i]['onout'],e)
+								if(thisAreas.status==1){
+									inEvent = 'out';
+									thisAreas.status = -1;
+									e.type = self.deviceType+'out';
+									if(self.regEvent[inEvent].status){
+										self.occur(thisAreas['onout'],e,thisAreas)
+									}
 								}
 							}
 						}
 					}
 					
 				}else{
-					//text.innerHTML += 'ADD ' + addEvent + '|'
-					self.canvas['on'+addEvent] = function(e){
+					self.canvas['on'+self.regEvent[inEvent].name] = function(e){
+						//获取当前鼠标在画布上的坐标
 						var offset = self.getEventOffset(e);
+						//遍历所有事件区域
 						for(var i=0;i<self.childLength;i++){
-							var isIn = self.areas[i].isInArea(offset.x,offset.y);
-							
-							
-							
+							var thisAreas = self.areas[i];
+							//鼠标是否在形状内
+							var isIn = thisAreas.isInArea(offset.x,offset.y);
 							if(isIn){
 								var type = e.type.replace(/mouse|touch/g, "");
-								
-								if(type=='start'){
-									self.occur(self.areas[i]['on'+'down'],e)
-								}else if(type=='end'){
-									self.occur(self.areas[i]['on'+'up'],e)
-								}else{
-									self.occur(self.areas[i]['on'+type],e)
+								//画布是否有注册当前事件
+								if(self.regEvent[inEvent].status){
+									self.occur(thisAreas['on'+inEvent],e,thisAreas)
 								}
-								
 							}
 						}
 					}
@@ -139,17 +127,25 @@
 			}
 			
 			
-			
-			
 		},
 		//禁用
 		disabled:function(){
-			var len = arguments.length;
-			var self = this;
+			var len = arguments.length,
+				self = this;
 			
 			for(var i=0;i<len;i++){
-				self.canvas['on'+arguments[i]] = null;
-				self.regEvent[arguments[i]] = false;
+				var inEvent = arguments[i];
+				if(!self.regEvent[inEvent].status)continue;
+				
+				self.regEvent[inEvent].status = false;
+				
+				if(inEvent=='move'||inEvent=='out'||inEvent=='over'){
+					if(!self.regEvent['move'].status&&!self.regEvent['out'].status&&!self.regEvent['over'].status){
+						self.canvas['on'+self.deviceType+'move'] = null;
+					}
+				}else{
+					self.canvas['on'+self.regEvent[inEvent].name] = null;
+				}
 			}
 		},
 		
@@ -189,9 +185,9 @@
 			return offset
 		},
 		
-		occur: function(func, e){
+		occur: function(func, e, thisAreas){
 			if(func && typeof func == "function"){
-				func(e);
+				func.call(thisAreas, e);
 			}
 		}
 	}
@@ -206,12 +202,18 @@
 		this.status = -1;
 		s.addChild(this);
 		
-		if(this.type='rect'){
+		if(this.type=='rect'){
 			this.width = option.width;
 			this.height = option.height;
+		}else if(this.type=='arc'){
+			var startD = option.startAngle<0?1:-1;
+			var endD = option.endAngle<0?1:-1;
+			this.radius = option.radius;
+			this.startAngle = option.startAngle+(Math.floor(Math.abs(option.startAngle)/360))*360*startD;
+			this.endAngle = option.endAngle+(Math.ceil(Math.abs(option.endAngle)/360))*360*endD;
 		}
 	}
-	
+
 	CE.Area.prototype = {
 		constructor: CE.Area,
 		addEvent:function(type, func){
@@ -229,12 +231,32 @@
 				case 'rect':
 					if(pageX>=x && pageX<= (x+this.width) && pageY >= y && pageY<=(y+this.height)){
 						return true;
+					}else{
+						return false;
+					}
+				case 'arc':
+					var dx = this.x - pageX;
+					var dy = this.y - pageY;
+					if(dx * dx + dy * dy > this.radius * this.radius)return false;
+					var thisA = 180/Math.PI*Math.atan(dy/dx);
+
+					if(pageX>=this.x&&pageY<=this.y){//第一象限
+						thisA = 360+thisA
+					}else if(pageX<=this.x&&pageY<=this.y){//第二象限
+						thisA = 180+thisA
+					}else if(pageX<=this.x&&pageY>=this.y){//第三象限
+						thisA = 180+thisA
+					}else if(pageX>=this.x&&pageY>=this.y){//第四象限
+						thisA = thisA
 					}
 					
-				case 'round':
-					return false
+					if(thisA>=this.startAngle&&thisA<=this.endAngle){
+						return true
+					}else{
+						return false
+					}
 			}
-			return false;
+			
 		}
 		
 		
